e8d4b952edfddf205a3af03b2aefca612b9d78fb
Caleb <7217571+cbuahin@users.noreply.github.com>
2022-02-19 11:01:33 -0500

    Update convlstm.py

--- convlstm.py
+++ convlstm.py
@@ -4,7 +4,14 @@ import torch
 
 class ConvLSTM1DCell(nn.Module):
 
-    def __init__(self, input_dim, hidden_dim, kernel_size, bias):
+    def __init__(
+            self,
+            input_dim,
+            hidden_dim,
+            kernel_size,
+            padding=0,
+            bias=True
+    ):
         """
         Initialize ConvLSTM cell.
         Parameters
@@ -25,7 +32,7 @@ class ConvLSTM1DCell(nn.Module):
         self.hidden_dim = hidden_dim
 
         self.kernel_size = kernel_size
-        self.padding = kernel_size // 2
+        self.padding = padding
         self.bias = bias
 
         self.conv = nn.Conv1d(in_channels=self.input_dim + self.hidden_dim,
@@ -52,63 +59,9 @@ class ConvLSTM1DCell(nn.Module):
         return h_next, c_next
 
     def init_hidden(self, batch_size, length):
+        out_length = length + 2 * self.padding - self.kernel_size + 1
         return (torch.zeros(batch_size, self.hidden_dim, length, device=self.conv.weight.device),
-                torch.zeros(batch_size, self.hidden_dim, length, device=self.conv.weight.device))
-
-
-class ConvLSTM2DCell(nn.Module):
-
-    def __init__(self, input_dim, hidden_dim, kernel_size, bias):
-        """
-        Initialize ConvLSTM cell.
-        Parameters
-        ----------
-        input_dim: int
-            Number of channels of input tensor.
-        hidden_dim: int
-            Number of channels of hidden state.
-        kernel_size: (int, int)
-            Size of the convolutional kernel.
-        bias: bool
-            Whether or not to add the bias.
-        """
-
-        super(ConvLSTM2DCell, self).__init__()
-
-        self.input_dim = input_dim
-        self.hidden_dim = hidden_dim
-
-        self.kernel_size = kernel_size
-        self.padding = kernel_size[0] // 2, kernel_size[1] // 2
-        self.bias = bias
-
-        self.conv = nn.Conv2d(in_channels=self.input_dim + self.hidden_dim,
-                              out_channels=4 * self.hidden_dim,
-                              kernel_size=self.kernel_size,
-                              padding=self.padding,
-                              bias=self.bias)
-
-    def forward(self, input_tensor, cur_state):
-        h_cur, c_cur = cur_state
-
-        combined = torch.cat([input_tensor, h_cur], dim=1)  # concatenate along channel axis
-
-        combined_conv = self.conv(combined)
-        cc_i, cc_f, cc_o, cc_g = torch.split(combined_conv, self.hidden_dim, dim=1)
-        i = torch.sigmoid(cc_i)
-        f = torch.sigmoid(cc_f)
-        o = torch.sigmoid(cc_o)
-        g = torch.tanh(cc_g)
-
-        c_next = f * c_cur + i * g
-        h_next = o * torch.tanh(c_next)
-
-        return h_next, c_next
-
-    def init_hidden(self, batch_size, image_size):
-        height, width = image_size
-        return (torch.zeros(batch_size, self.hidden_dim, height, width, device=self.conv.weight.device),
-                torch.zeros(batch_size, self.hidden_dim, height, width, device=self.conv.weight.device))
+                torch.zeros(batch_size, self.hidden_dim, out_length, device=self.conv.weight.device))
 
 
 class ConvLSTM1D(nn.Module):
@@ -136,8 +89,16 @@ class ConvLSTM1D(nn.Module):
         >> h = last_states[0][0]  # 0 for layer index, 0 for h index
     """
 
-    def __init__(self, input_dim, hidden_dim, kernel_size, num_layers,
-                 batch_first=False, bias=True, return_all_layers=False):
+    def __init__(
+            self,
+            input_dim,
+            hidden_dim,
+            kernel_size,
+            num_layers=1,
+            batch_first=False,
+            bias=True,
+            return_all_layers=False
+    ):
         super(ConvLSTM1D, self).__init__()
 
         self._check_kernel_size_consistency(kernel_size)
@@ -239,6 +200,73 @@ class ConvLSTM1D(nn.Module):
         return param
 
 
+class ConvLSTM2DCell(nn.Module):
+
+    def __init__(
+            self,
+            input_dim,
+            hidden_dim,
+            kernel_size,
+            padding=(0, 0),
+            bias=True
+    ):
+        """
+        Initialize ConvLSTM cell.
+        Parameters
+        ----------
+        input_dim: int
+            Number of channels of input tensor.
+        hidden_dim: int
+            Number of channels of hidden state.
+        kernel_size: (int, int)
+            Size of the convolutional kernel.
+        bias: bool
+            Whether or not to add the bias.
+        """
+
+        super(ConvLSTM2DCell, self).__init__()
+
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
+
+        self.kernel_size = kernel_size
+        self.padding = padding
+        self.bias = bias
+
+        self.conv = nn.Conv2d(in_channels=self.input_dim + self.hidden_dim,
+                              out_channels=4 * self.hidden_dim,
+                              kernel_size=self.kernel_size,
+                              padding=self.padding,
+                              bias=self.bias)
+
+    def forward(self, input_tensor, cur_state):
+        h_cur, c_cur = cur_state
+
+        combined = torch.cat([input_tensor, h_cur], dim=1)  # concatenate along channel axis
+
+        combined_conv = self.conv(combined)
+        cc_i, cc_f, cc_o, cc_g = torch.split(combined_conv, self.hidden_dim, dim=1)
+        i = torch.sigmoid(cc_i)
+        f = torch.sigmoid(cc_f)
+        o = torch.sigmoid(cc_o)
+        g = torch.tanh(cc_g)
+
+        c_next = f * c_cur + i * g
+        h_next = o * torch.tanh(c_next)
+
+        return h_next, c_next
+
+    def init_hidden(self, batch_size, image_size):
+        
+        height, width = image_size
+        
+        out_height = height + 2 * self.padding[0] - self.kernel_size[0] + 1
+        out_width = width + 2 * self.padding[1] - self.kernel_size[1] + 1
+
+        return (torch.zeros(batch_size, self.hidden_dim, height, width, device=self.conv.weight.device),
+                torch.zeros(batch_size, self.hidden_dim, out_height, out_width, device=self.conv.weight.device))
+
+
 class ConvLSTM2D(nn.Module):
     """
     Parameters:
@@ -259,13 +287,23 @@ class ConvLSTM2D(nn.Module):
                     each element of the list is a tuple (h, c) for hidden state and memory
     Example:
         >> x = torch.rand((32, 10, 64, 128, 128))
-        >> convlstm = ConvLSTM(64, 16, 3, 1, True, True, False)
+        >> convlstm = ConvLSTM(64, 16, (3,3), 1, True, True, False)
         >> _, last_states = convlstm(x)
         >> h = last_states[0][0]  # 0 for layer index, 0 for h index
     """
 
-    def __init__(self, input_dim, hidden_dim, kernel_size, num_layers,
-                 batch_first=False, bias=True, return_all_layers=False):
+    def __init__(
+            self,
+            input_dim,
+            hidden_dim,
+            kernel_size,
+            padding=(0, 0),
+            num_layers=1,
+            batch_first=False,
+            bias=True,
+            return_all_layers=False
+    ):
+
         super(ConvLSTM2D, self).__init__()
 
         self._check_kernel_size_consistency(kernel_size)
@@ -273,12 +311,15 @@ class ConvLSTM2D(nn.Module):
         # Make sure that both `kernel_size` and `hidden_dim` are lists having len == num_layers
         kernel_size = self._extend_for_multilayer(kernel_size, num_layers)
         hidden_dim = self._extend_for_multilayer(hidden_dim, num_layers)
+        padding = self._extend_for_multilayer(padding, num_layers)
+
         if not len(kernel_size) == len(hidden_dim) == num_layers:
             raise ValueError('Inconsistent list length.')
 
         self.input_dim = input_dim
         self.hidden_dim = hidden_dim
         self.kernel_size = kernel_size
+        self.padding = padding
         self.num_layers = num_layers
         self.batch_first = batch_first
         self.bias = bias
@@ -291,6 +332,7 @@ class ConvLSTM2D(nn.Module):
             cell_list.append(ConvLSTM2DCell(input_dim=cur_input_dim,
                                             hidden_dim=self.hidden_dim[i],
                                             kernel_size=self.kernel_size[i],
+                                            padding=self.padding[i],
                                             bias=self.bias))
 
         self.cell_list = nn.ModuleList(cell_list)
